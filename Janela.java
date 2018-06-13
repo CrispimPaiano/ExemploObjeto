b74492005b3dee6d1b050460dfd614360b92a31a
CrispimPaiano <crispimpds5@gmail.com>
2018-06-13 19:35:16 -0300

    Get e Set Janela.java
    resta somente criar o JUnitsTest

--- Janela.java
+++ Janela.java
@@ -8,4 +8,52 @@ public class Janela {
     private String marca;
     private char possuiInsulfilm;
     
+    public void setComprimento (float comprimento){
+        this.comprimento = comprimento;
+    }
+    
+    public float getComprimento(){
+        return comprimento;
+    }
+    
+    public void setLargura (float largura){
+        this.largura = largura;
+    }
+    
+    public float getLargura(){
+        return largura;
+    }
+    
+    public void setAltura (float altura){
+        this.altura = altura;
+    }
+    
+    public float getAltura(){
+        return altura;
+    }
+    
+    public void setPreco (double preco){
+        this.preco = preco;
+    }
+    
+    public double getPreco(){
+        return preco;
+    }
+    
+    public void setMarca (String marca){
+        this.marca = marca;
+    }
+    
+    public String getMarca(){
+        return marca;
+    }
+    
+    public void setPossuiInsulfilm(char possuiInsulfim){
+        this.possuiInsulfilm = possuiInsulfim;
+    }
+    
+    public char getPossuiInsulfilm(){
+        return possuiInsulfilm;
+    }
+    
 }
